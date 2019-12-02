b67f07b5d4756f6247ad0289a28aa694679bfe33
yehui <hye@today36524.com.cn>
2019-12-02 15:19:09 +0800

    s

--- hystrix-service/src/main/java/com/macro/cloud/HystrixServiceApplication.java
+++ hystrix-service/src/main/java/com/macro/cloud/HystrixServiceApplication.java
@@ -2,14 +2,16 @@ package com.macro.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
-public class UserServiceApplication {
+public class HystrixServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(HystrixServiceApplication.class, args);
     }
 
 }
