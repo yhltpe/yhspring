6fff3fc640e19ba2b17b828926926a5039ba32cf
yehui <hye@today36524.com.cn>
2019-12-04 13:37:44 +0800

    ss

---
+++ feign-service/src/main/java/com/macro/cloud/service/UserServiceFeign.java
@@ -0,0 +1,28 @@
+package com.macro.cloud.service;
+
+import com.macro.cloud.domain.CommonResult;
+import com.macro.cloud.domain.User;
+import com.macro.cloud.service.impl.UserFallbackService;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by macro on 2019/9/5.
+ */
+@FeignClient(value = "user-service",fallback = UserFallbackService.class)
+public interface UserServiceFeign {
+    @PostMapping("/user/create")
+    CommonResult create(@RequestBody User user);
+
+    @GetMapping("/user/{id}")
+    CommonResult<User> getUser(@PathVariable(value = "id") Long id);
+
+    @GetMapping("/user/getByUsername")
+    CommonResult<User> getByUsername(@RequestParam(name="username") String username);
+
+    @PostMapping("/user/update")
+    CommonResult update(@RequestBody User user);
+
+    @PostMapping("/user/delete/{id}")
+    CommonResult delete(@PathVariable(value = "id") Long id);
+}
